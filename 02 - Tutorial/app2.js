decb93b4d0b31e8384f4d6bf81f9fb00683bcb07
Maja Blagic <majablgic505@gmail.com>
2021-08-06 22:23:18 +0200

    Kyeboard Events

---
+++ 02 - Tutorial/app2.js
@@ -0,0 +1,17 @@
+new Vue({
+    el:'#vue-app2',
+    data:{
+      
+
+    },
+    methods:{
+        logName:function(){
+            console.log('You entered your name');
+        },
+        logAge:function(){
+            console.log('You entered your age');
+        },
+
+    },
+
+});
