4e45487d9fa09d76e2f815312f4c60f6994ccde7
Maja Blagic <majablgic505@gmail.com>
2021-08-06 16:33:09 +0200

    Events

--- 02 - Tutorial/app.js
+++ 02 - Tutorial/app.js
@@ -5,6 +5,10 @@ new Vue({
       job: 'Ninja',
       website: 'https://naruto.fandom.com/wiki/Kakashi_Hatake',
       websiteTag: '<a href="https://naruto.fandom.com/wiki/Kakashi_Hatake">Bio</a> ',
+      age: 35,
+      x: 0,
+      y:0,
+
   },
   methods:{ //this is also an object in which we can store as much methods as we want
 
@@ -15,6 +19,17 @@ new Vue({
             return 'Good Day ' + this.name
         }
         return 'こんにちわ ' + this.name
+    },
+    add:function(year){
+        this.age += year;
+    },
+    subtract:function(year){
+        this.age -= year;
+    },
+    updateXY(event){
+        this.x = event.offsetX;
+        this.y = event.offsetY;
+        
     }
 
   }
