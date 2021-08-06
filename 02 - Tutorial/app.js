dbfab3bb7ea020134546ff01e65359b298843664
Maja Blagic <majablgic505@gmail.com>
2021-08-06 14:44:07 +0200

    Data and Methods

--- 02 - Tutorial/app.js
+++ 02 - Tutorial/app.js
@@ -1,6 +1,21 @@
 new Vue({
   el:'#vue-app',
   data:{
-      name: 'Sam'
+      name: 'Kakashi',
+      job: 'Ninja',
+
+  },
+  methods:{ //this is also an object in which we can store as much methods as we want
+
+    greet:function(language){
+
+
+        if(language == 'English'){
+            return 'Good Day ' + this.name
+        }
+        return 'こんにちわ ' + this.name
+    }
+
   }
+  
 });
